94869c36da3f926f76dd513fa0989193c08dd95f
JonGudenzi <jongudenzi@gmail.com>
2022-02-01 22:44:12 -0500

    storing user name and score data. Creating a list forEach object in the local storage array

--- script.js
+++ script.js
@@ -1,181 +1,171 @@
-var timerDiv = document.querySelector("#timer") ;
+var timerDiv = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
-var quizArea = document.querySelector("#container"); 
+var quizArea = document.querySelector("#container");
 var highScores = document.querySelector("#highScores");
 var counter = document.querySelector("#counter");
 var choiceDiv = document.querySelector("#choices");
 var userNameEl = document.querySelector("#userName");
-var choiceA = document.querySelector("#A")
-var choiceB = document.querySelector("#B")
-var choiceC = document.querySelector("#C")
+var choiceA = document.querySelector("#A");
+var choiceB = document.querySelector("#B");
+var choiceC = document.querySelector("#C");
 var question = document.getElementById("question");
-var writeUserNameAndScore = document.getElementById("writeUserNameAndScore");
-var questionBox = document.getElementById("question-box")
+// var writeUserNameAndScore = document.getElementById("writeUserNameAndScore");
+var questionBox = document.getElementById("question-box");
 var score = 0;
-var user = document.getElementById('userName');
-var lastQuestion = currentQuestion.length -1;
+var user = document.getElementById("userName");
+var lastQuestion = currentQuestion.length - 1;
 var runningQuestion = 0;
 var count = 8;
 var TIMER;
+var stores = JSON.parse(localStorage.getItem("database")) || [];
+//   var newStores = [stores];
 
 // question
-function renderQuestion(){
-    var q = currentQuestion[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
+function renderQuestion() {
+  var q = currentQuestion[runningQuestion];
 
-
+  question.innerHTML = "<p>" + q.question + "</p>";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
 }
 
-startBtn.addEventListener("click",startQuiz);
+startBtn.addEventListener("click", startQuiz);
 
 // start quiz
-function startQuiz(){
-    timerDiv.style.display = "block";
-    choiceDiv.style.display = "flex";
-    question.style.display = "block";
-    highScores.style.display = "none";
-    start.style.display = "none";
-    renderQuestion();
-    quizArea.style.display = "block";
-    questionBox.style.display = "block";
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000); 
+function startQuiz() {
+  timerDiv.style.display = "block";
+  choiceDiv.style.display = "flex";
+  question.style.display = "block";
+  highScores.style.display = "none";
+  start.style.display = "none";
+  renderQuestion();
+  quizArea.style.display = "block";
+  questionBox.style.display = "block";
+  renderCounter();
+  TIMER = setInterval(renderCounter, 1000);
 }
 
 // timer and ending of quiz if timer runs out
-function renderCounter(){
-   
-    if( count >= 0){
-        counter.innerHTML = count;
-        count--
-    }else{
-        count <= 0;
-        answerIsWrong();
-        scoreRender();
-    
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-           
-        }else{
-            console.log("renderCounter")
-            clearInterval(TIMER);
+function renderCounter() {
+  if (count >= 0) {
+    counter.innerHTML = count;
+    count--;
+  } else {
+    count <= 0;
+    answerIsWrong();
+    scoreRender();
 
-        }
+    if (runningQuestion < lastQuestion) {
+      runningQuestion++;
+      renderQuestion();
+    } else {
+      console.log("renderCounter");
+      clearInterval(TIMER);
     }
+  }
 }
 
 // check to see if answer is right or wrong
-function checkAnswer(answer){
-    if( answer == currentQuestion[runningQuestion].correct){
-        score++;
-        answerIsCorrect();
-        
+function checkAnswer(answer) {
+  if (answer == currentQuestion[runningQuestion].correct) {
+    score++;
+    answerIsCorrect();
+  } else {
+    answerIsWrong();
+  }
 
-    }else{
-        answerIsWrong();
-    }
-    
-    
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestion();
-    }else{
-        clearInterval(TIMER);
-        scoreRender();
-    }
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
+    renderQuestion();
+  } else {
+    clearInterval(TIMER);
+    scoreRender();
+  }
 }
 
 //  correct
-function answerIsCorrect(){
-    document.getElementById(runningQuestion);
-   
+function answerIsCorrect() {
+  document.getElementById(runningQuestion);
 }
 
 //  Wrong
-function answerIsWrong(){
-    document.getElementById(runningQuestion);
-    count -=5;
+function answerIsWrong() {
+  document.getElementById(runningQuestion);
+  count -= 5;
 }
-
 
 // end quiz
-function scoreRender(){
-    highScores.style.display = "block";
-    start.style.display = "none";
-    question.style.display = "none";
-    questionBox.style.display = "none";
-    timerDiv.style.display = "none";
-    choiceDiv.style.display = "none";   
+function scoreRender() {
+  highScores.style.display = "block";
+  start.style.display = "none";
+  question.style.display = "none";
+  questionBox.style.display = "none";
+  timerDiv.style.display = "none";
+  choiceDiv.style.display = "none";
+  
 
-    if (localStorage.getItem("database") == null) {
-        document.getElementById('writeUserNameAndScore').innerHTML = "nothing stored.";
-    } else {
-        document.getElementById('writeUserNameAndScore').innerHTML = JSON.parse(localStorage.getItem("database"));
+if (localStorage.getItem("database") == null) {
+    document.getElementById('writeUserNameAndScore').innerHTML = "nothing stored.";
+} else {
+    document.getElementById('writeUserNameAndScore').innerHTML = JSON.parse(localStorage.getItem("database"));
 
-    }
-
-
-
-    
 }
 
+}
 
 function saveUserScore() {
-var newStores = JSON.parse(localStorage.getItem('database')) || "";
-var stores = [newStores];
-
-    var scorePerCent = Math.round(100 * score/currentQuestion.length) + '%';
     
 
-    var saveUserScore = user.value;
-    if ((saveUserScore == null) || (saveUserScore == "")) {
-        document.getElementById('writeUserNameAndScore').innerHTML = "nothing to store.";
-    } else {
-        
-        stores.push(saveUserScore);
-        stores.push(scorePerCent);
+  var scorePerCent = Math.round((100 * score) / currentQuestion.length) + "%";
+  
+  var saveUserScore = user.value;
+  if (saveUserScore == null || saveUserScore == "") {
+    document.getElementById("writeUserNameAndScore").innerHTML =
+      "nothing to store.";
+  }
+  
+else
+    {
+        document.getElementById("writeUserNameAndScore").innerHTML = "";
 
-        //clear the input field for visual 
-        user.value = "";
-        //print value in local storage
-          localStorage.setItem("database", JSON.stringify(stores.join(" ")));
-        
-        //confirm write
-        document.getElementById('writeUserNameAndScore').innerHTML = stores;
-        
-        
-        if (localStorage.getItem("database") == null) {
-            document.getElementById('writeUserNameAndScore').innerHTML = "nothing stored.";
-        } else {
-            
-            document.getElementById('writeUserNameAndScore').innerHTML = JSON.parse(localStorage.getItem("database"));
+        stores.push({
+    user: saveUserScore,
+    score: scorePerCent
+});
 
-            
-        }
+      //clear the input field for visual
+      user.value = "";
+      //print value in local storage
+      localStorage.setItem("database", JSON.stringify(stores));
 
+      //confirm write
+    //   document.getElementById("writeUserNameAndScore").innerHTML = stores;
 
-        
-
+      
+        // document.getElementById("writeUserNameAndScore").innerHTML = JSON.parse(
+        //   localStorage.getItem("database")
+        // );
+        listData ();
+        $('input').remove();
+        $('#saveScore').remove();
+      
     }
-
-
-
-};
-
-// function readStatus() {
     
-    
-//     var scorePerCent = Math.round(100 * score/currentQuestion.length);
-    
-    // if (window.localStorage.getItem("database") == null) {
-    //     document.getElementById('writeUserNameAndScore').innerHTML = "nothing stored.";
-    // } else {
-    //     document.getElementById('writeUserNameAndScore').innerHTML = window.localStorage.getItem("database") + ' ' + scorePerCent + '%'
+  };
 
-    // }
+  function listData (){
+
+    if (localStorage.getItem("database") == null) {
+        document.getElementById("saveScore").innerHTML =
+          "nothing to store.";
+      } else {
+
+    var dataBaseEl = $('<ul></ul>');
+stores.forEach(function(val){
+    dataBaseEl.append('<li>' + val.user + ' ' + val.score +'</li>');
+});
+dataBaseEl.insertAfter($('#writeUserNameAndScore'));
+  }
+}
 
